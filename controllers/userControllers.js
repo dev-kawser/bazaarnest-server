af38276d5e8edb7d3b142b04b45d938293f0e708
dev-kawser <kawserofficial.dev@gmail.com>
2024-11-29 01:15:00 +0600

    Add: add update user api for update user informations

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -1,6 +1,7 @@
-const { client } = require("../config/database")
+const { client, ObjectId } = require("../config/database")
 const usersCollection = client.db("hektoDB").collection("users");
 
+// Post a new user
 exports.postUser = async (req, res) => {
     try {
         const user = req.body;
@@ -21,6 +22,7 @@ exports.postUser = async (req, res) => {
     }
 };
 
+// Get a user by email
 exports.getUserByEmail = async (req, res) => {
     try {
         const email = req.params.email;
@@ -34,6 +36,25 @@ exports.getUserByEmail = async (req, res) => {
         res.status(200).json(user);
     } catch (error) {
         console.error("Error fetching user by email:", error);
+        res.status(500).json({ message: "Internal server error" });
+    }
+};
+
+// Update a user data
+exports.updateUser = async (req, res) => {
+    try {
+        const id = req.params.id;
+        const update = { $set: req.body };
+
+        const result = await usersCollection.updateOne({ _id: new ObjectId(id) }, update);
+
+        if (result.modifiedCount === 0) {
+            return res.status(404).json({ message: "User not found" });
+        }
+
+        res.status(200).json({ message: "User updated successfully" });
+    } catch (error) {
+        console.error("Error updating user:", error);
         res.status(500).json({ message: "Internal server error" });
     }
 };
--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,9 @@
 const express = require("express");
-const { postUser, getUserByEmail } = require("../controllers/userControllers");
+const { postUser, getUserByEmail, updateUser } = require("../controllers/userControllers");
 const router = express.Router();
 
 router.post("/", postUser);
 router.get("/:email", getUserByEmail);
+router.put("/updateUser/:id", updateUser);
 
 module.exports = router;
