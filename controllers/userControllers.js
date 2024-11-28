0dd2718c3ee3878a03f918eaf3a46914132ba5b1
dev-kawser <kawserofficial.dev@gmail.com>
2024-11-29 00:32:20 +0600

    Add: add get user by email api for getting current user by email query

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -20,3 +20,20 @@ exports.postUser = async (req, res) => {
         res.status(500).json({ message: "Error creating user", error });
     }
 };
+
+exports.getUserByEmail = async (req, res) => {
+    try {
+        const email = req.params.email;
+        const query = { email };
+        const user = await usersCollection.findOne(query);
+
+        if (!user) {
+            return res.status(404).json({ message: "User not found" });
+        }
+
+        res.status(200).json(user);
+    } catch (error) {
+        console.error("Error fetching user by email:", error);
+        res.status(500).json({ message: "Internal server error" });
+    }
+};
--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,8 @@
 const express = require("express");
-const { postUser } = require("../controllers/userControllers");
+const { postUser, getUserByEmail } = require("../controllers/userControllers");
 const router = express.Router();
 
 router.post("/", postUser);
+router.get("/:email", getUserByEmail);
 
 module.exports = router;
