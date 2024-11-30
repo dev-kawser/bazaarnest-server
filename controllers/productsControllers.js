d48178eeb0f3b048638b0acec77be37a988f119f
dev-kawser <kawserofficial.dev@gmail.com>
2024-11-30 19:36:55 +0600

    Add: add an api for get all products

--- controllers/productsControllers.js
+++ controllers/productsControllers.js
@@ -1,2 +1,13 @@
 const { client, ObjectId } = require("../config/database")
 const productsCollection = client.db("hektoDB").collection("products");
+
+// get all products
+exports.getAllProducts = async (req, res) => {
+    try {
+        const products = await productsCollection.find().toArray();
+        res.status(200).json(products);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: "Error fetching products" });
+    }
+};
--- routes/products.js
+++ routes/products.js
@@ -1,4 +1,7 @@
 const express = require("express");
+const { getAllProducts } = require("../controllers/productsControllers");
 const router = express.Router();
+
+router.get('/', getAllProducts);
 
 module.exports = router;
