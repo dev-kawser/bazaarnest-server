6b33c372b07831d680a87eb5930a0d552063f267
dev-kawser <kawserofficial.dev@gmail.com>
2024-11-30 19:57:08 +0600

    Add: add an api for get products by id

--- controllers/productsControllers.js
+++ controllers/productsControllers.js
@@ -11,3 +11,21 @@ exports.getAllProducts = async (req, res) => {
         res.status(500).json({ message: "Error fetching products" });
     }
 };
+
+// get product by ID
+exports.getProductById = async (req, res) => {
+    const productId = req.params.id;
+
+    try {
+        const product = await productsCollection.findOne({ _id: new ObjectId(productId) });
+
+        if (!product) {
+            return res.status(404).json({ message: "Product not found" });
+        }
+
+        res.status(200).json(product);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: "Error fetching product" });
+    }
+};
--- routes/products.js
+++ routes/products.js
@@ -1,7 +1,8 @@
 const express = require("express");
-const { getAllProducts } = require("../controllers/productsControllers");
+const { getAllProducts, getProductById } = require("../controllers/productsControllers");
 const router = express.Router();
 
 router.get('/', getAllProducts);
+router.get('/:id', getProductById);
 
 module.exports = router;
