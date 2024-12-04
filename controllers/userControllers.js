2ae8875b30e44d33dd5b0927b04c0fe6afd2b0f3
dev-kawser <kawserofficial.dev@gmail.com>
2024-12-04 22:52:02 +0600

    Add: add an api for get all users

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -58,3 +58,14 @@ exports.updateUser = async (req, res) => {
         res.status(500).json({ message: "Internal server error" });
     }
 };
+
+// Get all users
+exports.getAllUsers = async (req, res) => {
+    try {
+        const users = await usersCollection.find().toArray();
+        res.status(200).json(users);
+    } catch (error) {
+        console.error("Error fetching all users:", error);
+        res.status(500).json({ message: "Internal server error" });
+    }
+};
--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,9 @@
 const express = require("express");
-const { postUser, getUserByEmail, updateUser } = require("../controllers/userControllers");
+const { postUser, getUserByEmail, updateUser, getAllUsers } = require("../controllers/userControllers");
 const router = express.Router();
 
 router.post("/", postUser);
+router.get("/", getAllUsers);
 router.get("/:email", getUserByEmail);
 router.put("/updateUser/:id", updateUser);
 
