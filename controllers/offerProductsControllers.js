e7da092e8df217e774181c24589584c21c23f5a9
dev-kawser <kawserofficial.dev@gmail.com>
2024-12-06 01:13:16 +0600

    Add: add an api for update offer product

--- controllers/offerProductsControllers.js
+++ controllers/offerProductsControllers.js
@@ -68,3 +68,32 @@ exports.deleteOfferProductById = async (req, res) => {
         res.status(500).json({ message: "Error deleting product" });
     }
 };
+
+// update product by ID
+exports.updateOfferProductById = async (req, res) => {
+    const productId = req.params.id;
+    const updatedProduct = req.body;
+
+    if (!updatedProduct) {
+        return res.status(400).json({ message: "Missing required fields" });
+    }
+
+    try {
+        // Exclude the `_id` field to prevent modification
+        const { _id, ...fieldsToUpdate } = updatedProduct;
+
+        const result = await offerProductsCollection.updateOne(
+            { _id: new ObjectId(productId) },
+            { $set: fieldsToUpdate }
+        );
+
+        if (result.modifiedCount === 0) {
+            return res.status(404).json({ message: "Offer Product not found or no changes made" });
+        }
+
+        res.status(200).json({ message: "Offer Product updated successfully" });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: "Error updating product" });
+    }
+};
--- routes/offerProducts.js
+++ routes/offerProducts.js
@@ -1,10 +1,11 @@
 const express = require("express");
-const { getAllOfferProducts, getOfferProductById, addOfferProduct, deleteOfferProductById } = require("../controllers/offerProductsControllers");
+const { getAllOfferProducts, getOfferProductById, addOfferProduct, deleteOfferProductById, updateOfferProductById } = require("../controllers/offerProductsControllers");
 const router = express.Router();
 
 router.post('/', addOfferProduct)
 router.get('/', getAllOfferProducts);
 router.get('/:id', getOfferProductById);
 router.delete('/delete/:id', deleteOfferProductById);
+router.patch('/update/:id', updateOfferProductById);
 
 module.exports = router;
